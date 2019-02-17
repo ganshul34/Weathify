aeebf49eed951b026a852e53807551fa25b80860
Anshul <ganshul34@gmail.com>
2019-02-18 00:42:40 +0530

    Added Initial Files

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Platform } from 'expo-core';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.largeText,styles.text]}> San Franscisco</Text>
+        <Text style={[styles.largeText,styles.text]}>Light Cloud</Text>
+        <Text style={[styles.largeText,styles.text]}>24°</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle:{
+    textAlign: 'center',
+    fontFamily: Platform.OS==='ios'? 'AvenirNext-Regular' : 'Roboto'
+  },
+  largeText:{
+    fontSize: 44, 
+  },
+  smallText:
+  {
+    fontSize:18,
+  },
+});
